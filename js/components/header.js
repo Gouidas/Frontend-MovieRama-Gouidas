a201c5eb8de02a0eda2483918ee22f4f626f01a5
Gouidas <t.gouidas@gmail.com>
2023-07-14 15:42:41 +0300

    test

--- js/components/header.js
+++ js/components/header.js
@@ -2,7 +2,7 @@ const headerContainer = document.getElementById("header-container");
 headerContainer.innerHTML = `
   <div class="flex-container navbar">
     <div>
-      <h1>MovieRama</h1>
+      <h1>MovieRama!</h1>
     </div>
     <div class="flex-container_center">
       <div class="flex-container_center">
